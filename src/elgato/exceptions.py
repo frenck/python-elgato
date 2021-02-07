a5efb78520846e9dbe8c7c77ccc3e1b6986e876b
Franck Nijhof <git@frenck.dev>
2021-02-07 20:44:51 +0100

    Quality assurance refactoring (#202)
    * Quality assurance refactoring
    
    * Add CodeQL
    
    * Tweak CodeQL
    
    * Reconfigure tests and more mypy strictness
    
    * Remove no longer needed makefile
    
    * Fix node in CI

--- src/elgato/exceptions.py
+++ src/elgato/exceptions.py
@@ -4,10 +4,6 @@
 class ElgatoError(Exception):
     """Generic Elgato Key Light exception."""
 
-    pass
-
 
 class ElgatoConnectionError(ElgatoError):
     """Elgato Key Light connection exception."""
-
-    pass
